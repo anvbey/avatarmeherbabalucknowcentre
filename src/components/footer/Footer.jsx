6bcfd7e6ed5bb596b7c1e55cbf72c41a07bed4ca
Anvit Dubey <anvitgogoa@gmail.com>
2024-02-17 03:18:38 +0530

    Added Navbar and Footer

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Box, Typography } from "@mui/material";
+
+function Footer() {
+  return (
+    <Box
+      sx={{
+        backgroundColor: "#000",
+        color: "#fff",
+        textAlign: "center",
+        padding: "10px 0",
+        height: "30px",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        position: "relative",
+        bottom: 0,
+        width: "100%",
+      }}
+    >
+      <Typography variant="body1">
+        © 2023 Avatar Meher Baba Lucknow Centre
+      </Typography>
+    </Box>
+  );
+}
+
+export default Footer;
