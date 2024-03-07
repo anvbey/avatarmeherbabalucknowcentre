8867c15fe9f9261b4aee8c305de571507df9bff3
AkarshDubey <akarshd98@gmail.com>
2024-03-08 04:07:26 +0530

    Added Routing and new pages including HighlightEvent and Registration. Also, converted the About component on home page to two separate About components - one for Meher Baba and other for Lucknow Centre.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-// import { Routes, Route } from "react-router-dom";
+import {Routes, Route } from "react-router-dom";
 import Footer from "./components/footer/Footer";
 import Navbar from "./components/navbar/Navbar";
 import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+import Contact from "./pages/Contact/Contact";
+import Events from "./pages/Events/Events";
+import Registration from "./pages/Registration/Registration";
+import HighlightEvent from "./pages/Events/HighlightEvent";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Home />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/events" element={<Events />} />
+        <Route path="/register" element={<Registration />} />
+        <Route path="/oct2024" element={<HighlightEvent />} />
+      </Routes>
+
       <Footer/>
     </div>
   );
---
+++ src/pages/About/About.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Box, Typography } from "@mui/material";
+
+const About = () => {
+  
+
+  return (
+    <Box sx={{  margin:'60px'}}>
+      <Typography variant="h3" sx={{ textAlign: "left" }}>
+        About // To be implemented
+      </Typography>
+
+      
+    </Box>
+  );
+};
+
+export default About;
