7e176d36a1a332334dde134dc7f868e086819be9
Anvit Dubey <anvitgogoa@gmail.com>
2024-03-10 11:49:57 +0530

    Added About page

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,16 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import { Box, Typography } from "@mui/material";
-
+import Overview from "../../components/about/Overview";
+import Timeline from "../../components/about/Timeline";
+import SideMenu from "../../components/about/SideMenu";
 const About = () => {
-  
+  const sections = [
+    { id: "section1", title: "Section 1" },
+    { id: "section2", title: "Section 2" },
+    { id: "section3", title: "Section 3" },
+    // Add more sections as needed
+  ];
 
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const handleMenuItemClick = (sectionId) => {
+    // Implement logic to scroll to the corresponding section based on sectionId
+    // You may use libraries like react-scroll for smooth scrolling
+    console.log(`Clicked on section: ${sectionId}`);
+  };
   return (
-    <Box sx={{  margin:'60px'}}>
-      <Typography variant="h3" sx={{ textAlign: "left" }}>
-        About // To be implemented
-      </Typography>
-
+    <Box sx={{ margin: "60px" }}>
       
+
+    <SideMenu isOpen={isMenuOpen} toggleMenu={toggleMenu} />
+    {/* Your page content goes here */}
+    <div className="content">
+      {/* Your content sections go here */}
+      <div id="section1">Section 1 Content</div>
+      <div id="section2">Section 2 Content</div>
+      <div id="section3">Section 3 Content</div>
+    </div>
     </Box>
   );
 };
