35927ca937f84674daf7df9b8f3b34746be0b279
Anvit Dubey <anvitgogoa@gmail.com>
2024-03-10 16:24:11 +0530

    Minor fixes
    - adding timeline in about page
    - adding card in events page

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,8 +1,10 @@
 import React from "react";
-import { Box } from "@mui/material";
+import { Box} from "@mui/material";
 import Overview from "../../components/about/Overview";
 import Timeline from "../../components/about/Timeline";
+import Present from "../../components/about/Present";
 const About = () => {
+  
 
   return (
     <Box sx={{ margin: "60px" }}>
