bc7b773951ac25db5fb5066d49fb5c48a95bd4c3
Aryan Srivastava <aryansri.19@gmail.com>
2024-04-03 21:28:40 +0530

    Revert "Registration Form (Full) (#6)" (#7)
    This reverts commit 3173375a8d8fe769375052f66408643b85257235.

--- backend/routes/form.route.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-const controller = require('../controllers/form.controller');
-
-router.post('/contact', controller.contactForm);
-router.post('/registration', controller.registrationForm);
-
-module.exports = router;
