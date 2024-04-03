c93345b1467c53d07718dbe2c58bec10339dd5f7
Aryan Srivastava <aryansri.19@gmail.com>
2024-04-03 21:29:40 +0530

    Registration Form (Full) (#8)
    * Some changes
    
    Temporary changes in registration form
    
    * Registration Form Page Complete
    
    -Form schema working fine.
    -Values logging successfully
    
    * Minor changes
    
    Server route for registration form setup
    
    * Registration form
    
    1. Remove button added
    2. Continue correctly working
    
    * About page side panel (#3)
    
    * Update README.md
    
    * Modified Home Page
    
    * Modified Home, HighlightEvent, About, Events Page
    
    - made all the components of the respective pages
    
    * Adding events data json schema
    
    * About page side panel
    
    ---------
    
    Co-authored-by: Anvit Dubey <73951320+anvbey@users.noreply.github.com>
    Co-authored-by: Anvit Dubey <anvitgogoa@gmail.com>
    
    * Registration Form changes
    
    1. Member Form Completed
    2. Database queries added
    
    * Registration Form Changes
    
    1. Database insertion
    2. Module format fixes
    
    ---------
    
    Co-authored-by: Shryesh Khandelwal <vrocks98@gmail.com>
    Co-authored-by: Anvit Dubey <73951320+anvbey@users.noreply.github.com>
    Co-authored-by: Anvit Dubey <anvitgogoa@gmail.com>

--- backend/controllers/form.controller.js
+++ backend/controllers/form.controller.js
@@ -1,22 +1,64 @@
-const { sendMail } = require('../utils/mail');
+const sendMail = require("../utils/mail");
+const pg = require('../database/pg');
 
 const contactForm = async (req, res) => {
-    const { name, email, phone, subject, message } = req.body;
-
-    try {
-        await sendMail('contactForm', { name, phone, message, date: new Date(), email, subject});
-        return res.status(200).json({ message: 'Email sent successfully' });
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({ message: 'Internal server error' });
-    }
-}
+  const { name, email, phone, subject, message } = req.body;
+  try {
+    await sendMail("contactForm", {
+      name,
+      phone,
+      message,
+      date: new Date(),
+      email,
+      subject,
+    });
+    return res.status(200).json({ message: "Email sent successfully" });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
 
 const registrationForm = async (req, res) => {
-    const { firstName, lastName, email, phone, city, age, gender, numberOfMembers, members } = req.body;
-}
+  const { firstName, lastName, email, phone, city, age, gender, members } =
+    req.body;
+  try {
+    await pg.query("BEGIN");
+    console.log(req.body)
+    const id = await pg.query(
+      `INSERT INTO registrations (first_name, last_name, email, phone, city, age, gender) values ($1, $2, $3, $4, $5, $6, $7) RETURNING id`,
+      [firstName, lastName, email, phone, city, age, gender]
+    );
+    const groupLeadId = id.rows[0].id;
+    await pg.query(
+      `INSERT INTO registrations (group_lead_id) values ($1) WHERE id = $2`,
+      [groupLeadId, groupLeadId]
+    );
+    members.forEach(async (member) => {
+      await pg.query(
+        `INSERT INTO members (first_name, last_name, email, phone, city, age, gender, group_lead_id) values ($1, $2, $3, $4, $5, $6, $7, $8)`,
+        [
+          member.firstName,
+          member.lastName,
+          member.email,
+          member.phone,
+          member.city,
+          member.age,
+          member.gender,
+          groupLeadId,
+        ]
+      );
+    });
+    await pg.query("COMMIT");
+    return res.status(200).json({ message: "Registration successful" });
+  } catch (error) {
+    await pg.query("ROLLBACK");
+    console.log(error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
 
 module.exports = {
-    contactForm,
-    registrationForm
+  contactForm,
+  registrationForm,
 };
