61a81235fa93c12f3ee4240fa317a31831061bc2
Aryan Srivastava <aryansri.19@gmail.com>
2024-04-18 12:56:27 +0530

    UI changes (#18)
    * Layout changes
    
    Registration page refactored
    
    * Fixing MenuIcon import in Navbar.jsx
    
    * Registration changes
    
    1. Update button added
    2. when number of members field is null, the member forms are stil visible
    
    * Update registration form
    
    1) Added Date and time of Arrival
    2) Added Date and time of Departure
    
    ---------
    
    Co-authored-by: Akarsh <apple@AMB-MacBook-Pro.local>

--- src/lib/formSchema/registrationFormSchema.jsx
+++ src/lib/formSchema/registrationFormSchema.jsx
@@ -8,15 +8,16 @@ const groupFormSchema = z.object({
     city: z.string().min(1, "Address is required"),
     age: z.coerce.number().min(1, "Age is required"),
     gender: z.string(),
+    dateOfArrival: z.string(),
+    dateOfDeparture: z.string(),
     numberOfMembers: z.coerce.number().min(1, "At least one member is required"),
     members: z.array(z.object({
         firstName: z.string().min(1, "First Name is required"),
         lastName: z.string().min(1, "Last Name is required"),
         gender: z.string(),
-        age: z.coerce.number().int().positive(),
-        email: z.string().email().optional(),
+        age: z.coerce.number().min(1, "Age is required"),
         phone: z.string().optional(),
-    })),
+    }).optional()),
 });
 
 export { groupFormSchema };
--- src/pages/Registration/Registration.jsx
+++ src/pages/Registration/Registration.jsx
@@ -1,16 +1,9 @@
 import React from 'react';
-import { Box } from "@mui/material";
 import GroupForm from './GroupLeaderForm';
 
 const Registration = () => {
     return (
-        <>
-        <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', padding: '5%'}}>
-            <Box sx={{ marginTop: '50px' }}>
-            <GroupForm/>
-            </Box>
-        </Box>
-        </>
+        <GroupForm/>
     );
 };
 
