ba77395801e840c6f8a686309253a30574e36f91
Aryan Srivastava <aryansri.19@gmail.com>
2024-09-25 17:45:28 +0000

    Fix: Removed warnings and updated dependencies

--- src/components/highlightEvent/AboutEvent.jsx
+++ src/components/highlightEvent/AboutEvent.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import { Box, Button, Typography } from "@mui/material";
-import { Link } from "react-router-dom";
+import { Box, Typography } from "@mui/material";
 import YouTubeEmbed from "./YoutubeVideo";
 import "./AboutEvent.css";
 import { useTranslation } from "react-i18next";
